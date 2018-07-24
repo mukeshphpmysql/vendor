b1f971328619591949a81acf04f4c85dc3a894d2
Mukesh Patel <mukesh.patel@perficient.com>
2018-07-24 17:27:51 +0530

    Create Controller in Magento 2

---
+++ Catalog/Controller/Adminhtml/Product/Index.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Mukesh_Catalog extension custom catalog
+ * @category  Mukesh
+ * @package   Mukesh_Catalog
+ * @author    Mukesh Patel
+ */
+namespace Mukesh\Catalog\Controller\Adminhtml\Product;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Mukesh_Catalog::products';
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Product list page
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Mukesh_Catalog::products');
+        $resultPage->getConfig()->getTitle()->prepend(__('Products'));
+
+        return $resultPage;
+    }
+
+    /**
+     * Check Permission.
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Mukesh_Catalog::products');
+    }
+}
