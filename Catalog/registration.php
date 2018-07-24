5fa06eefaf49c470a8a69e247a3c490201ace60a
Mukesh Patel <mukesh.patel@perficient.com>
2018-07-24 12:21:33 +0530

    How to add menu in admin magento

---
+++ Catalog/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Mukesh_Catalog extension custom catalog
+ * @category  Mukesh
+ * @package   Mukesh_Catalog
+ * @author    Mukesh Patel
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mukesh_Catalog',
+    __DIR__
+);
